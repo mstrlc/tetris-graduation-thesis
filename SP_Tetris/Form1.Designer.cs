024907200fa51d3493d9c4f20e9227f12fefe9dd
Matyáš Strelec <matyasstrelec@gmail.com>
2021-02-23 01:07:25 +0100

    Add project files.

---
+++ SP_Tetris/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace SP_Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonFall = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonRotateCounterClockwise = new System.Windows.Forms.Button();
+            this.buttonRotateClockwise = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(286, 540);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(363, 13);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonFall
+            // 
+            this.buttonFall.Location = new System.Drawing.Point(363, 42);
+            this.buttonFall.Name = "buttonFall";
+            this.buttonFall.Size = new System.Drawing.Size(75, 23);
+            this.buttonFall.TabIndex = 2;
+            this.buttonFall.Text = "fall";
+            this.buttonFall.UseVisualStyleBackColor = true;
+            this.buttonFall.Click += new System.EventHandler(this.buttonFall_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(326, 84);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.Text = "left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(363, 113);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.Text = "right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonRotateCounterClockwise
+            // 
+            this.buttonRotateCounterClockwise.Location = new System.Drawing.Point(326, 222);
+            this.buttonRotateCounterClockwise.Name = "buttonRotateCounterClockwise";
+            this.buttonRotateCounterClockwise.Size = new System.Drawing.Size(75, 23);
+            this.buttonRotateCounterClockwise.TabIndex = 7;
+            this.buttonRotateCounterClockwise.Text = "rotate ccw";
+            this.buttonRotateCounterClockwise.Click += new System.EventHandler(this.buttonRotateCounterClockwise_Click);
+            // 
+            // buttonRotateClockwise
+            // 
+            this.buttonRotateClockwise.Location = new System.Drawing.Point(363, 193);
+            this.buttonRotateClockwise.Name = "buttonRotateClockwise";
+            this.buttonRotateClockwise.Size = new System.Drawing.Size(75, 23);
+            this.buttonRotateClockwise.TabIndex = 6;
+            this.buttonRotateClockwise.Text = "rotate cw";
+            this.buttonRotateClockwise.UseVisualStyleBackColor = true;
+            this.buttonRotateClockwise.Click += new System.EventHandler(this.buttonRotateClock_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 565);
+            this.Controls.Add(this.buttonRotateClockwise);
+            this.Controls.Add(this.buttonRotateCounterClockwise);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonFall);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonFall;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonRotateCounterClockwise;
+        private System.Windows.Forms.Button buttonRotateClockwise;
+    }
+}
+
